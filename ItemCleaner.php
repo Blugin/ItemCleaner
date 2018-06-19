4bfe9b329e8dc2af0665dbb57eda9938cf71fc98
KimSangYun <32999975+UnixTeamAdmin@users.noreply.github.com>
2018-06-19 21:17:33 +0900

    Bump API 3.0.0

--- ItemCleaner.php
+++ ItemCleaner.php
@@ -4,8 +4,8 @@
  * @name ItemCleaner
  * @main ItemCleaner\Main
  * @author BLEAN
- * @version 1.0
- * @api 3.0.0-ALPHA12
+ * @version 1.1
+ * @api 3.0.0
  */
 
 namespace ItemCleaner;
@@ -20,30 +20,34 @@ class Main extends \pocketmine\plugin\PluginBase{
 	public $time = 0;
 	public $count = 0;
 	public function onEnable(){
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new class($this) extends \pocketmine\scheduler\PluginTask{
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new class($this) extends \pocketmine\scheduler\Task{
+			public $owner;
+			public function __construct(Main $owner){
+				$this->owner = $owner;
+			}
 			public function onRun(int $currentTick){
-				++$this->getOwner()->time;
-				if ($this->getOwner()->time == 1){
-					$this->getOwner()->getServer()->broadcastMessage($this->getOwner()->prefix . 'Items will be removed after 3 minute.');
+				++$this->owner->time;
+				if ($this->owner->time == 1){
+					$this->owner->getServer()->broadcastMessage($this->owner->prefix . 'Items will be removed after 3 minute.');
 				}
-				if ($this->getOwner()->time == 2){
-					$this->getOwner()->getServer()->broadcastMessage($this->getOwner()->prefix . 'Items will be removed after 2 minute.');
+				if ($this->owner->time == 2){
+					$this->owner->getServer()->broadcastMessage($this->owner->prefix . 'Items will be removed after 2 minute.');
 				}
-				if ($this->getOwner()->time == 3){
-					$this->getOwner()->getServer()->broadcastMessage($this->getOwner()->prefix . 'Items will be removed after 1 minute.');
+				if ($this->owner->time == 3){
+					$this->owner->getServer()->broadcastMessage($this->owner->prefix . 'Items will be removed after 1 minute.');
 				}
-				if ($this->getOwner()->time == 4){
-					foreach ($this->getOwner()->getServer()->getLevels() as $levels){
+				if ($this->owner->time == 4){
+					foreach ($this->owner->getServer()->getLevels() as $levels){
 						foreach($levels->getEntities() as $entities){
 							if (!$entities instanceof Creature && !$entities instanceof Human){
 								$entities->close();
-								++$this->getOwner()->count;
+								++$this->owner->count;
 							}
 						}
 					}
-					$this->getOwner()->getServer()->broadcastMessage($this->getOwner()->prefix . '§d' . $this->getOwner()->count . ' §fItems have been deleted.');
-					$this->getOwner()->time = 0;
-					$this->getOwner()->count = 0;
+					$this->owner->getServer()->broadcastMessage($this->owner->prefix . '§d' . $this->owner->count . ' §fItems have been deleted.');
+					$this->owner->time = 0;
+					$this->owner->count = 0;
 				}
 			}
 		}, 20 * 60);
