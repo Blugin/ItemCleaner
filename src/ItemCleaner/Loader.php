2379ef482602aeb94391dfe1d35ce6aa772a4541
KimSangYun <32999975+UnixTeamAdmin@users.noreply.github.com>
2018-07-09 22:27:16 +0900

    shit

--- src/ItemCleaner/Loader.php
+++ src/ItemCleaner/Loader.php
@@ -7,16 +7,17 @@ use pocketmine\plugin\PluginBase;
 use ItemCleaner\Task\CheckTask;
 
 class Loader extends PluginBase{
-	
 	/** @var string */
-    	public $prefix = '§d[ §fCleaner §d]§f ';
+	public $prefix = '§d[ §fCleaner §d]§f ';
 
-    	public function onEnable(): void {
+	public function onEnable(): void {
 		@mkdir($this->getDataFolder());
+
 		$this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML, [
 			'time' => 5
 		]);
 		$this->db['config'] = $this->config->getAll();
+		
 		$this->getScheduler()->scheduleRepeatingTask(new CheckTask($this), 20 * 60);
 	}
 }
