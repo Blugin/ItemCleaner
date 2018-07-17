a30de559530cfc2c360f1f51bc9d44620b9e59bf
PresentKim <qjrmckd@gmail.com>
2018-07-18 02:28:01 +0900

    CheckTask : Store removed entity count into local variable instead of a property

--- src/ItemCleaner/Task/CheckTask.php
+++ src/ItemCleaner/Task/CheckTask.php
@@ -17,9 +17,6 @@ class CheckTask extends Task{
     /** @var int */
     private $time;
 
-    /** @var int */
-    private $entityCount;
-
     public function __construct(Loader $owner){
         $this->owner = $owner;
         $this->time = $this->owner->db['config']['time'];
@@ -28,17 +25,17 @@ class CheckTask extends Task{
         if ($this->time != 0){
             $this->owner->getServer()->broadcastMessage($this->owner->prefix . 'Items will be removed after §d' . --$this->time . '§f minute.');
         }else{
+			$entityCount = 0;
             foreach ($this->owner->getServer()->getLevels() as $levels){
                 foreach($levels->getEntities() as $entities){
                     if (!$entities instanceof Creature && !$entities instanceof Human){
                         $entities->close();
-                        ++$this->entityCount;
+                        ++$entityCount;
                     }
                 }
             }
-            $this->owner->getServer()->broadcastMessage($this->owner->prefix . '§d' . $this->entityCount . ' §fItems have been deleted.');
+            $this->owner->getServer()->broadcastMessage($this->owner->prefix . '§d' . $entityCount . ' §fItems have been deleted.');
             $this->time = $this->owner->db['config']['time'];
-            $this->entityCount = 0;
         }
     }
 }
