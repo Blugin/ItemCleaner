d39782623180b25ef3e07c3ca27206d7b1a90cef
PresentKim <qjrmckd@gmail.com>
2018-07-18 02:27:58 +0900

    Loader : Use reloadConfig() and getConfig()

--- src/ItemCleaner/Loader.php
+++ src/ItemCleaner/Loader.php
@@ -13,12 +13,8 @@ class Loader extends PluginBase{
 	public $prefix = '§d[ §fCleaner §d]§f ';
 
 	public function onEnable(): void {
-		@mkdir($this->getDataFolder());
-
-		$this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML, [
-			'time' => 5
-		]);
-		$this->db['config'] = $this->config->getAll();
+		$this->reloadConfig();
+		$this->db['config'] = $this->getConfig()->getAll();
 		
 		$this->getScheduler()->scheduleRepeatingTask(new CheckTask($this), 20 * 60);
 	}
